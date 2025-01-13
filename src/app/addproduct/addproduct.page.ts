3efec1813ccbc885877281769caecd03859bf4a7
pakin sukkho <pakinsukko@gmail.com>
2025-01-13 11:43:39 +0700

    first commit

---
+++ src/app/addproduct/addproduct.page.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton } from '@ionic/angular/standalone';
+import { DataapiService } from '../dataapi.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-addproduct',
+  templateUrl: './addproduct.page.html',
+  styleUrls: ['./addproduct.page.scss'],
+  standalone: true,
+  imports: [IonButton, IonInput, IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class AddproductPage implements OnInit {
+
+  txtname: string = '';
+  txtprice: string = '';
+  selectFile: File | null = null;
+  dataapi: any;
+
+  constructor(
+    private dataApiService: DataapiService,
+    private router: Router
+  ) { }
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectFile = file;
+    }
+  }
+
+  ngOnInit() { }
+
+  addproduct() {
+    const formData = new FormData();
+    formData.append('name', this.txtname);
+    formData.append('price', this.txtprice);
+
+    if (this.selectFile) {
+      formData.append('image', this.selectFile, this.selectFile.name);
+    }
+
+    this.dataApiService.addproduct(formData).subscribe({
+      next: (res: any) => {
+        console.log("บันทึกข้อมูลสำเร็จ", res);
+        this.txtname = '';
+        this.txtprice = '';
+        this.selectFile = null;
+      },
+      error: (err) => {
+        console.error("บันทึกข้อมูลไม่สำเร็จ", err);
+      }
+    });
+  }
+}
---
+++ src/app/dataapi.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataapiService {
+
+  constructor(
+    public http: HttpClient
+  ) { }
+
+
+    //ฟังก์ชั่นเชื่อมต่อ api ระบบบันทึกข้อมูล
+    addproduct(formData: any) {
+      return this.http.post('http://127.0.0.1/class2-4/crudapi/upload.php', formData);
+    }  
+
+
+}
